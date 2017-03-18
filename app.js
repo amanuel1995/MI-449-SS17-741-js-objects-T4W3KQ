c633570d12f18da44efb17f4bb933afa5744ebc5
Amanuel <amaniye1@live.com>
2017-03-18 02:49:46 -0400

    completed all functinalities. Add/remove/search jokes and remember

--- app.js
+++ app.js
@@ -8,22 +8,41 @@ var deleteThisJoke = document.getElementById('delete-button')
 var jokeSavedNotice = document.getElementById('saved-notice')
 var jokeDeletedNotice = document.getElementById('delete-notice')
 var addJokeAtIndex = 0
+var stringifiedJokes
+var stringifiedJokesArray
+var jokes = {}
+var arrayOfJokes = []
+var initFlag = 0
 
-// A couple jokes to start with
-var jokes = {
-  'the horse': {
-    setup: 'A horse walks into the bar. The bartender asks...',
-    punchline: 'Why the long face?'
-  },
-  'Orion\'s pants': {
-    setup: 'How does Orion keep his pants up?',
-    punchline: 'With an asteroid belt.'
-  }
+stringifiedJokes = JSON.stringify(jokes)
+stringifiedJokesArray = JSON.stringify(arrayOfJokes)
+
+if (parseInt(window.localStorage.getItem('flag')) !== 1) {
+  window.localStorage.setItem('arrayOfJokes', stringifiedJokesArray)
+  window.localStorage.setItem('jokes', stringifiedJokes)
 }
 
-// create array to store keys in it
-var arrayOfJokes = ['the horse', 'Orion\'s pants']
-
+// A couple jokes to start with or parse localStorage
+if (window.localStorage.getItem('jokes') === '{}') {
+  jokes = {
+    'the horse': {
+      setup: 'A horse walks into the bar. The bartender asks...',
+      punchline: 'Why the long face?'
+    },
+    'Orion\'s pants': {
+      setup: 'How does Orion keep his pants up?',
+      punchline: 'With an asteroid belt.'
+    }
+  }
+} else {
+  jokes = JSON.parse(window.localStorage.getItem('jokes'))
+}
+// create or retrieve existing array to store the joke keys
+if (window.localStorage.getItem('arrayOfJokes') === '[]') {
+  arrayOfJokes = ['the horse', 'Orion\'s pants']
+} else {
+  arrayOfJokes = JSON.parse(window.localStorage.getItem('arrayOfJokes'))
+}
 // The message to display if the jokes object is empty
 var noJokesMessage = 'I... I don\'t know any jokes. 😢'
 
@@ -39,7 +58,7 @@ var updateJokesMenu = function () {
 var requestedJokeInput = document.getElementById('requested-joke')
 var jokeBox = document.getElementById('joke-box')
 
-// function to check existing joke and display
+// function to check existing joke and display it when searched for
 var updateDisplayedJoke = function () {
   if ((requestedJokeInput.value !== null) && (requestedJokeInput.value !== '')) {
     var i
@@ -58,18 +77,6 @@ var updateDisplayedJoke = function () {
   }
 }
 
-// Function to keep track of all page updates to  call them all at once
-var updatePage = function () {
-  updateJokesMenu()
-  updateDisplayedJoke()
-}
-
-// Update the page immediately on startup
-updatePage()
-
-// var stringifiedJokes = window.localStorage.getItem('jokes')
-// jokes = JSON.parse(stringifiedJokes)
-
 // add new joke object to jokes object/struct
 var saveNewJoke = function () {
   // create new joke object inside jokes
@@ -86,6 +93,14 @@ var saveNewJoke = function () {
 
   // update jokes menu to include new joke
   updateJokesMenu()
+
+  // update localStorage with modified version
+  stringifiedJokes = JSON.stringify(jokes)
+  stringifiedJokesArray = JSON.stringify(arrayOfJokes)
+  window.localStorage.setItem('arrayOfJokes', stringifiedJokesArray)
+  window.localStorage.setItem('jokes', stringifiedJokes)
+  initFlag = 1
+  window.localStorage.setItem('flag', initFlag)
 }
 
 // Delete unwanted jokes
@@ -94,7 +109,9 @@ var deleteJokeObject = function () {
   var j
   for (j = 0; j < arrayOfJokes.length; j++) {
     if (arrayOfJokes[j] === deleteJokeKeyValue) {
+      // delete the joke from jokes obect using that key
       delete jokes[deleteJokeKeyValue]
+      // update arrayOfJokes by removing the item
       var indexDeleteKey = arrayOfJokes.indexOf(deleteJokeKeyValue)
       if (indexDeleteKey !== -1) {
         arrayOfJokes.splice(indexDeleteKey, 1)
@@ -103,17 +120,30 @@ var deleteJokeObject = function () {
 
       // update the menu list
       updateJokesMenu()
+
+      // update localStorage with modified version
+      stringifiedJokes = JSON.stringify(jokes)
+      stringifiedJokesArray = JSON.stringify(arrayOfJokes)
+      window.localStorage.setItem('arrayOfJokes', stringifiedJokesArray)
+      window.localStorage.setItem('jokes', stringifiedJokes)
+      initFlag = 1
+      window.localStorage.setItem('flag', initFlag)
     } else {
       jokeDeletedNotice.innerHTML = 'That joke didn\'t exist.'
     }
   }
 }
 
-// save updated version of jokes in the localstorage
-// var stringifiedJokes = JSON.stringify(jokes)
-// window.localStorage.setItem('jokes', stringifiedJokes)
+// Function to keep track of all page updates to  call them all at once
+var updatePage = function () {
+  updateJokesMenu()
+  updateDisplayedJoke()
+}
 
-// Keep the requested joke up-to-date with event listeners
+// Update the page immediately on startup
+updatePage()
+
+// Keep the jokes up-to-date with event listeners
 requestedJokeInput.addEventListener('input', updateDisplayedJoke)
 addNewJoke.addEventListener('click', saveNewJoke)
 deleteThisJoke.addEventListener('click', deleteJokeObject)
